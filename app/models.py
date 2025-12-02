deeb447ca93a964af2acf8049e299846cd333f8a
Daniel <codedbydaniel@gmail.com>
2025-12-02 11:01:52 +0630

    first

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class PostModel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    viewer = models.IntegerField(default=1)
+    image = models.ImageField(null=True, upload_to="posts/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
---
+++ app/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render, redirect
+from app.models import PostModel, CategoryModel
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def post_list(request):
+    posts = PostModel.objects.all()
+    return render(request, "post_list.html", {"posts": posts})
+
+
+def post_create(request):
+    if request.method == "GET":
+        return render(request, "post_create.html")
+    if request.method == "POST":
+        title = request.POST.get("title")
+        viewer = request.POST.get("viewer")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+
+        post = PostModel.objects.create(
+            title=title, description=description, viewer=viewer, image=image
+        )
+        post.save()
+        return redirect("/post/list/")
+
+
+def post_update(request, pk):
+    post = PostModel.objects.get(id=pk)
+    if request.method == "GET":
+        return render(request, "post_update.html", {"post": post})
+    if request.method == "POST":
+        title = request.POST.get("title")
+        viewer = request.POST.get("viewer")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+
+        post.title = title
+        post.description = description
+        post.viewer = viewer
+        if image:
+            if post.image:
+                post.image.delete()
+            post.image = image
+        post.save()
+        return redirect("/post/list/")
+
+
+def post_delete(request, pk):
+    post = PostModel.objects.get(id=pk)
+    if request.method == "GET":
+        return render(request, "post_delete.html", {"post": post})
+    if request.method == "POST":
+        if post.image:
+            post.image.delete()
+        post.delete()
+        return redirect("/post/list/")
+
+
+def category_list(request):
+    categories = CategoryModel.objects.all()
+    return render(request, "category_list.html", {"categories": categories})
+
+
+def category_create(request):
+    if request.method == "GET":
+        return render(request, "category_create.html")
+    if request.method == "POST":
+        name = request.POST.get("name")
+
+        category = CategoryModel.objects.create(name=name)
+        category.save()
+        return redirect("/category/list/")
+
+
+def category_update(request, pk):
+    category = CategoryModel.objects.get(id=pk)
+    if request.method == "GET":
+        return render(request, "category_update.html", {"category": category})
+    if request.method == "POST":
+        name = request.POST.get("name")
+        category.name = name
+        category.save()
+        return redirect("/category/list/")
+
+
+def category_delete(request, pk):
+    category = CategoryModel.objects.get(id=pk)
+    if request.method == "GET":
+        return render(request, "category_delete.html", {"category": category})
+    if request.method == "POST":
+        category.delete()
+        return redirect("/category/list/")
